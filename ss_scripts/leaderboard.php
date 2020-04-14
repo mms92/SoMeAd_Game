25e3839dc27258d7ebdd059dcad39f2420ae2fd9
Mms92 <martin-manche@hotmail.fr>
2020-04-14 20:46:42 +0200

    update explode delimiter

--- ss_scripts/leaderboard.php
+++ ss_scripts/leaderboard.php
@@ -2,7 +2,7 @@
 if ( $_SERVER["REQUEST_METHOD"] == "GET" )
 {
     $url = $_SERVER["PHP_SELF"];
-    $segment = explode(" ", $url);
+    $segment = explode("\/", $url);
     echo json_encode( $segment );
 }
 ?>
