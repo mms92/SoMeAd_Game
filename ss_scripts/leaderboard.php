35c55693e3ba495a4a8921504b16a978b19b08d1
Mms92 <martin-manche@hotmail.fr>
2020-04-23 16:12:34 +0200

    Update leaderboard.php

--- ss_scripts/leaderboard.php
+++ ss_scripts/leaderboard.php
@@ -5,13 +5,24 @@ if ( $_SERVER["REQUEST_METHOD"] == "GET" )
     header('Content-Type: application/json');
     $url = $_SERVER["PHP_SELF"];
     $segment = explode("/", $url);
+
     $db = new SQL( "SoMeAd","abc123" );
-    $results = $db->getLeaderBoard();
-    if ($results != NULL) {
-        $results_array = $results->fetchAll();
-        echo json_encode( $results_array );
-    } else {
-        echo "0 results";
+    if ( isset($_REQUEST["name"]) && isset($_REQUEST["score"]) )
+    {
+        $results = $db->getLeaderBoardAround($_REQUEST["name"],$_REQUEST["score"]);
+        if ($results != NULL) {
+            $results_array = $results->fetchAll();
+            echo json_encode( $results_array );
+        }
     }
+    else
+    {
+        $results = $db->getLeaderBoard();
+        if ($results != NULL) {
+            $results_array = $results->fetchAll();
+            echo json_encode( $results_array );
+        }
+    }
+    
 }
 ?>
