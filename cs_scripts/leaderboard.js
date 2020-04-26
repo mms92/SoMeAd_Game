f07b11acaa02a6f52c81516cc86f2ac43f5f6489
Mms92 <martin-manche@hotmail.fr>
2020-04-26 17:59:55 +0200

    Update leaderboard.js

--- cs_scripts/leaderboard.js
+++ cs_scripts/leaderboard.js
@@ -10,15 +10,16 @@ class leaderboard {
         description.innerText = "Tableau des scores"
 
         this.tableBody = document.createElement("tbody")
-
+        this.tableBody.id = "leaderboard_body"
         this.leaderboardDiv.appendChild( description )
         this.leaderboardDiv.appendChild( this.tableBody )
+        document.body.appendChild( this.leaderboardDiv )
         this.getData()
     }
     createRow( rank, name, avatar, score )
     {
         var tableRow = document.createElement("tr")
-
+        tableRow.className = "leaderboard_entry"
         var rankcell = document.createElement("td")
         var namecell = document.createElement("td")
         var avatarcell = document.createElement("td")
@@ -28,12 +29,12 @@ class leaderboard {
         tableRow.appendChild( namecell );
         tableRow.appendChild( scorecell  );
         tableRow.appendChild( avatarcell );
-
+        
         this.tableBody.appendChild( tableRow );
 
         rankcell.innerText = rank
         namecell.innerText = name
-        avatarcell.innerHTML = "<img src='images/"+avatar+"' style=\"width:64px\; height:64px;\">"
+        avatarcell.innerHTML = "<img class='leaderboard_avatar' src='images/"+avatar+"' style=\"width:64px\; height:64px;\">"
         scorecell.innerText = score
     }
     getData()
