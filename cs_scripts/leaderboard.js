852400b8531820fb8ac8b849be8e7e85b5282682
Mms92 <martin-manche@hotmail.fr>
2020-04-26 13:09:07 +0200

    update

--- cs_scripts/leaderboard.js
+++ cs_scripts/leaderboard.js
@@ -1,6 +1,53 @@
 class leaderboard {
+    leaderboardDiv = null;
+    tableBody = null;
     begin()
     {
+        document.body.innerHTML = ""
+        this.leaderboardDiv = document.createElement("table")
 
+        var description = document.createElement("caption")
+        description.innerText = "Tableau des scores"
+
+        this.tableBody = document.createElement("tbody")
+
+        this.leaderboardDiv.appendChild( description )
+        this.leaderboardDiv.appendChild( tableHeader )
+        this.getData()
+    }
+    createRow( rank, name, avatar, score )
+    {
+        var tableRow = document.createElement("tr")
+
+        var rankcell = document.createElement("td")
+        var namecell = document.createElement("td")
+        var avatarcell = document.createElement("td")
+        var scorecell = document.createElement("td")
+
+        tableRow.appendChild( rankcell );
+        tableRow.appendChild( namecell );
+        tableRow.appendChild( scorecell  );
+        tableRow.appendChild( avatarcell );
+
+        this.tableBody.appendChild( tableRow );
+
+        rankcell.innerText = rank
+        namecell.innerText = name
+        avatarcell.innerHTML = "<img src='images/"+avatar+"' style=\"width:64px\; height:64px;\">"
+        scorecell.innerText = score
+    }
+    getData()
+    {
+        var xhttp = new XMLHttpRequest()
+        xhttp.onreadystatechange = function()
+        {
+
+            data = JSON.parse( this.responseText )
+            for (const row of data) {
+                createRow( row.rank, row.name, row.avatar, row.score )
+            }
+        };
+        xhttp.open("GET", "ss_scripts/sql.php/leaderboard/?id="+Game.id, true)
+        xhttp.send()
     }
 }
