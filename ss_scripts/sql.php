6a0913c656e02610d0f22b09120e5d0b3bdfaa48
Mms92 <martin-manche@hotmail.fr>
2020-04-26 03:23:16 +0200

    Update sql.php

--- ss_scripts/sql.php
+++ ss_scripts/sql.php
@@ -1,9 +1,28 @@
 ﻿<?php
+
+function json_encode_with_error( $data )
+{
+    $json = json_encode($data);
+    if ($json === false) {
+        // Avoid echo of empty string (which is invalid JSON), and
+        // JSONify the error message instead:
+        $json = json_encode(["jsonError" => json_last_error_msg()]);
+        if ($json === false) {
+            // This should not happen, but we go all the way now:
+            $json = '{"jsonError":"unknown"}';
+        }
+        // Set HTTP response status code to: 500 - Internal Server Error
+        http_response_code(500);
+    }
+    return $json;
+}
+
+
 if ( $_SERVER["REQUEST_METHOD"] == "GET" )
 {
     require_once "lib/database.php";
-    header('Content-Type: application/json; charset=utf-8');
-    echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
+    header('Content-Type: application/json; charset=utf-8;');
+    //echo "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />";
     $url = $_SERVER["PHP_SELF"];
     $segment = explode("/", $url);
     $db = new SQL( "SoMeAd","abc123" );
@@ -26,7 +45,7 @@ if ( $_SERVER["REQUEST_METHOD"] == "GET" )
             $subAction = $segment[ 5 ];
             if ( $subAction == "begin" && isset($_REQUEST["name"]) && isset($_REQUEST["avatar"] ) )
             {
-                echo json_encode($db->beginSession($_REQUEST["name"],$_REQUEST["avatar"]));
+                echo json_encode_with_error($db->beginSession($_REQUEST["name"],$_REQUEST["avatar"]));
             }
             if ( isset( $_REQUEST["id"] ) )
             {
@@ -34,21 +53,24 @@ if ( $_SERVER["REQUEST_METHOD"] == "GET" )
                 {
                     $question = $db->getQuestion($_REQUEST["id"]);
                     var_dump($question);
+                    echo json_encode_with_error( $question );
                 }
                 if ( $subAction == "getAnswers" )
                 {
                     $answers = $db->getAnswers($_REQUEST["id"]);
                     var_dump($answers);
+                    echo json_encode_with_error( $answers );
                 }
                 if ( $subAction == "verifyAnswer" && isset($_REQUEST["answersId"] ) )
                 {
-                    //echo json_encode($db->answerCheck($_REQUEST["id"],$_REQUEST["answersId"]));
+                    echo json_encode_with_error( "error: undeclared function")
+                    //echo json_encode_with_error($db->answerCheck($_REQUEST["id"],$_REQUEST["answersId"]));
                 }
             }
         }
         elseif ( isset($_REQUEST["id"]) )
         {
-            echo json_encode( $db->getSession($_REQUEST["id"])[0] );
+            echo json_encode_with_error( $db->getSession($_REQUEST["id"])[0] );
         }
     }
 }
